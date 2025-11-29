b56bc4bad816c3d7b955a9f59e20db0cb9c314d3
Fariha Promi <farihapromi2611@gmail.com>
2025-11-29 20:06:31 +0600

    Update my bookings page

--- components/StationCard.tsx
+++ components/StationCard.tsx
@@ -5,14 +5,12 @@ export default function StationCard({ station }: { station: any }) {
   if (!station?.id) return null; // safety check
 
   return (
-    <div className='p-6 bg-white rounded-2xl shadow-lg hover:shadow-xl transition transform hover:-translate-y-1 hover:scale-105 duration-300'>
+    <div className='p-6 bg-gray-300 rounded-2xl shadow-lg hover:shadow-xl transition transform hover:-translate-y-1 hover:scale-105 duration-300'>
       <div className='flex flex-col h-full justify-between'>
         <div>
-          <h3 className='text-xl font-bold text-gray-800 mb-2'>
-            {station.name}
-          </h3>
-          <p className='text-gray-500 mb-4'>{station.address}</p>
-          <p className='text-sm text-gray-400'>Capacity: {station.capacity}</p>
+          <h3 className='text-xl font-bold text-black mb-2'>{station.name}</h3>
+          <p className='text-black font-bold mb-4'>{station.address}</p>
+          <p className='text-sm text-black'>Capacity: {station.capacity}</p>
         </div>
 
         <Link
